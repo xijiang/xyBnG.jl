d53e61f7d4e27fa535878970cf03ca42d38fc6df
Documenter.jl <documenter@juliadocs.github.io>
2024-05-03 11:46:24 +0000

    build based on 589b93e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = xyBnG","category":"page"},{"location":"#xyBnG","page":"Home","title":"xyBnG","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for xyBnG.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [xyBnG]","category":"page"}]
+}
