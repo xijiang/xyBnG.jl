76c1fb6b804bd14b71ae6270ad82ac67476a4d80
Xijiang Yu <xijiang@users.noreply.github.com>
2024-09-11 09:07:55 +0200

    Phasing out breeding schemes
    - move Sync.sh to examples
    - preparing move xps to examples
    - savepar to Sum

--- src/xps/schemes/cull.jl
+++ src/xps/schemes/cull.jl
@@ -1,40 +1,4 @@
 """
-    savepar(dic, scenario)
-Save the parameters of the current simulation into file `scenario`.
-"""
-function savepar(scenario, file)
-    n = 0
-    for k in keys(scenario)
-        n = max(n, length(string(k)))
-    end
-    ios, iom = IOBuffer(), IOBuffer()
-
-    for (k, v) in pairs(scenario)
-        if typeof(v) ∈ (Cattle, Trait, Plan, Species)
-            println(iom)
-            println(iom, "[$k]")
-            println(iom, v)
-        elseif occursin("Tuple", string(typeof(v)))
-            println(iom, "\n[Schemes]")
-            for s in v
-                println(iom, "         ", string(s))
-            end
-        else
-            println(ios, lpad(k, n), ": ", v)
-        end
-    end
-    open(file, "w") do io
-        println(io, lpad("Started", n), ": ", time())
-        # use Dates.unix2datetime(time()) to convert above time to DateTime
-        print(io, String(take!(ios)))
-        print(io, String(take!(iom)))
-    end
-    foo = dirname(@__DIR__) * "/../sum/summary.ipynb"
-    bar = dirname(file) * "/sums.ipynb"
-    cp(foo, bar, force = true)
-end
-
-"""
     randbrd(test, foo, bar, lmp, ngn, trait, plan)
 Random selection on `foo`.xy, `foo`.ped in directory `test` for `ngn`
 generations. This is to select a single trait `trait` using a selection plan
