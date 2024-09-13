2925efdae5a9406028eb88a79d3e2fe00b101d73
Xijiang Yu <xijiang@users.noreply.github.com>
2024-09-13 12:01:25 +0200

    Extract chromosomes from a Base.
    The base can be a merged xy and lmp files from tskit.

--- src/xy.jl
+++ src/xy.jl
@@ -563,4 +563,30 @@ function tr8bit(fxy::AbstractString, bxy::AbstractString; bs = 2^16)
     end
 end
 
+"""
+    extchr(base::AbstractString, dest::AbstractString, chr::Int...)
+Extract chromosome `chr`s from directory `base` to `dest`.
+"""
+function extchr(base::AbstractString, dest::AbstractString, chr::Int...)
+    isdir(dest) || mkpath(dest)
+    name, nid = begin
+        lines = readlines("$base/desc.txt")
+        open("$dest/desc.txt", "w") do io
+            println(io, lines[1])
+            println(io, lines[2])
+        end
+        lines[1], parse(Int, lines[2])
+    end
+    lmp = deserialize("$base/$name.lmp")
+    tmp = filter(row -> row.chr in chr, lmp)
+    nlc = size(tmp, 1)
+    hps = mapit("$base/$name.xy")
+    open("$dest/$name.xy", "w") do io
+        hdr = header("$base/$name.xy")
+        write(io, Ref(hdr), [nlc, 2nid])
+        write(io, hps[1:nlc, :])
+    end
+    serialize("$dest/$name.lmp", tmp)
+end
+
 end # module XY
